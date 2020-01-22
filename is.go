5cb97519d19c4c355752af24f167744db52be6c8
Billy Zaelani malik <m.billyzaelani@gmail.com>
2020-01-22 16:09:31 +0700

    Add interface T based on interface TB in package testing

---
+++ is.go
@@ -0,0 +1,20 @@
+package is
+
+// T is the interface common to testing type.
+type T interface {
+	Error(args ...interface{})
+	Errorf(format string, args ...interface{})
+	Fail()
+	FailNow()
+	Failed() bool
+	Fatal(args ...interface{})
+	Fatalf(format string, args ...interface{})
+	Log(args ...interface{})
+	Logf(format string, args ...interface{})
+	Name() string
+	Skip(args ...interface{})
+	SkipNow()
+	Skipf(format string, args ...interface{})
+	Skipped() bool
+	Helper()
+}
