eda8d12d8baece94eae50fb7bcdab0ba30511e61
Billy Zaelani malik <m.billyzaelani@gmail.com>
2020-01-25 18:16:57 +0700

    Change test to it's own package

--- is_test.go
+++ is_test.go
@@ -1,8 +1,9 @@
-package is
+package is_test
 
 import (
 	"errors"
 	"fmt"
+	"github.com/billyzaelani/is"
 	"testing"
 )
 
@@ -33,42 +34,42 @@ func (m *mockT) out() string                               { return m.s }
 func TestEqual(t *testing.T) {
 	tests := []struct {
 		Name string
-		Got  func(is *Is)
+		Got  func(is *is.Is)
 		Want string
 	}{
 		{
 			Name: "equal",
-			Got:  func(is *Is) { is.Equal(1, 1) },
+			Got:  func(is *is.Is) { is.Equal(1, 1) },
 			Want: ``,
 		},
 		{
 			Name: "not equal",
-			Got:  func(is *Is) { is.Equal(1, 2) },
+			Got:  func(is *is.Is) { is.Equal(1, 2) },
 			Want: `1 != 2`,
 		},
 		{
 			Name: "both nil",
-			Got:  func(is *Is) { is.Equal(nil, nil) },
+			Got:  func(is *is.Is) { is.Equal(nil, nil) },
 			Want: ``,
 		},
 		{
 			Name: "different data type",
-			Got:  func(is *Is) { is.Equal(3, false) },
+			Got:  func(is *is.Is) { is.Equal(3, false) },
 			Want: `int(3) != bool(false)`,
 		},
 		{
 			Name: "specific integer",
-			Got:  func(is *Is) { is.Equal(int32(1), int64(2)) },
+			Got:  func(is *is.Is) { is.Equal(int32(1), int64(2)) },
 			Want: `int32(1) != int64(2)`,
 		},
 		{
 			Name: "with nil",
-			Got:  func(is *Is) { is.Equal(nil, "nil") },
+			Got:  func(is *is.Is) { is.Equal(nil, "nil") },
 			Want: `<nil> != string(nil)`,
 		},
 		{
 			Name: "nil slice",
-			Got: func(is *Is) {
+			Got: func(is *is.Is) {
 				var a []string
 				b := []string{"one", "two"}
 				is.Equal(a, b)
@@ -77,12 +78,12 @@ func TestEqual(t *testing.T) {
 		},
 		{
 			Name: "nil with slice",
-			Got:  func(is *Is) { is.Equal(nil, []string{"one", "two"}) },
+			Got:  func(is *is.Is) { is.Equal(nil, []string{"one", "two"}) },
 			Want: `<nil> != []string([one two])`,
 		},
 		{
 			Name: "with comment",
-			Got: func(is *Is) {
+			Got: func(is *is.Is) {
 				is.Equal("foo", "bar") // foo is not bar
 			},
 			Want: "foo != bar // foo is not bar",
@@ -92,7 +93,7 @@ func TestEqual(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.Name, func(t *testing.T) {
 			m := &mockT{}
-			is := New(m)
+			is := is.New(m)
 			tt.Got(is)
 			got := m.out()
 			want := tt.Want
@@ -107,12 +108,12 @@ func TestEqual(t *testing.T) {
 func TestNoErr(t *testing.T) {
 	tests := []struct {
 		Name string
-		Got  func(is *Is)
+		Got  func(is *is.Is)
 		Want string
 	}{
 		{
 			Name: "no error",
-			Got: func(is *Is) {
+			Got: func(is *is.Is) {
 				var err error
 				is.NoErr(err)
 			},
@@ -120,12 +121,12 @@ func TestNoErr(t *testing.T) {
 		},
 		{
 			Name: "error",
-			Got:  func(is *Is) { is.NoErr(errors.New("something's wrong")) },
+			Got:  func(is *is.Is) { is.NoErr(errors.New("something's wrong")) },
 			Want: `err: something's wrong`,
 		},
 		{
 			Name: "error with comment",
-			Got: func(is *Is) {
+			Got: func(is *is.Is) {
 				is.NoErr(errors.New("something's wrong")) // shouldn't be error
 			},
 			Want: `err: something's wrong // shouldn't be error`,
@@ -135,7 +136,7 @@ func TestNoErr(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.Name, func(t *testing.T) {
 			m := &mockT{}
-			is := New(m)
+			is := is.New(m)
 			tt.Got(is)
 			got := m.out()
 			want := tt.Want
@@ -150,33 +151,33 @@ func TestNoErr(t *testing.T) {
 func TestTrue(t *testing.T) {
 	tests := []struct {
 		Name string
-		Got  func(is *Is)
+		Got  func(is *is.Is)
 		Want string
 	}{
 		{
 			Name: "true",
-			Got: func(is *Is) {
+			Got: func(is *is.Is) {
 				is.True(1 == 1) // true
 			},
 			Want: ``,
 		},
 		{
 			Name: "false",
-			Got: func(is *Is) {
+			Got: func(is *is.Is) {
 				is.True(1 == 2) // comment
 			},
 			Want: `false: 1 == 2 // comment`,
 		},
 		{
 			Name: "extra parentheses",
-			Got: func(is *Is) {
+			Got: func(is *is.Is) {
 				is.True((1 == 2)) // comment
 			},
 			Want: `false: (1 == 2) // comment`,
 		},
 		{
 			Name: "new line",
-			Got: func(is *Is) {
+			Got: func(is *is.Is) {
 				is.True((1 == 2) &&
 					false)
 			},
@@ -184,7 +185,7 @@ func TestTrue(t *testing.T) {
 		},
 		{
 			Name: "multi line",
-			Got: func(is *Is) {
+			Got: func(is *is.Is) {
 				is.True((1 == 2) &&
 					false ||
 					false)
@@ -193,7 +194,7 @@ func TestTrue(t *testing.T) {
 		},
 		{
 			Name: "multi line with comment in first line",
-			Got: func(is *Is) {
+			Got: func(is *is.Is) {
 				is.True((1 == 2) && // comment
 					false ||
 					false)
@@ -202,7 +203,7 @@ func TestTrue(t *testing.T) {
 		},
 		{
 			Name: "multi line with comment in non-first line",
-			Got: func(is *Is) {
+			Got: func(is *is.Is) {
 				is.True((1 == 2) &&
 					false || // cannot be printed
 					false)
@@ -214,7 +215,7 @@ func TestTrue(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.Name, func(t *testing.T) {
 			m := &mockT{}
-			is := New(m)
+			is := is.New(m)
 			tt.Got(is)
 			got := m.out()
 			want := tt.Want
@@ -229,22 +230,22 @@ func TestTrue(t *testing.T) {
 func TestLine(t *testing.T) {
 	tests := []struct {
 		Name string
-		Got  func(is *Is)
+		Got  func(is *is.Is)
 		Want int
 	}{
 		{
 			Name: "Equal",
-			Got:  func(is *Is) { is.Equal(1, 2) },
+			Got:  func(is *is.Is) { is.Equal(1, 2) },
 			Want: 2,
 		},
 		{
 			Name: "NoErr",
-			Got:  func(is *Is) { is.NoErr(errors.New("something's wrong")) },
+			Got:  func(is *is.Is) { is.NoErr(errors.New("something's wrong")) },
 			Want: 2,
 		},
 		{
 			Name: "True",
-			Got:  func(is *Is) { is.True(1 == 2) },
+			Got:  func(is *is.Is) { is.True(1 == 2) },
 			Want: 2,
 		},
 	}
@@ -252,7 +253,7 @@ func TestLine(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.Name, func(t *testing.T) {
 			m := &mockT{}
-			is := New(m)
+			is := is.New(m)
 			tt.Got(is)
 			got := m.helperCount
 			want := tt.Want
