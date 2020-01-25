f94e261e00cb5edab502b958bd9edc098dcb0c73
Billy Zaelani malik <m.billyzaelani@gmail.com>
2020-01-25 19:08:09 +0700

    Add documentation and example

--- is.go
+++ is.go
@@ -1,3 +1,19 @@
+// Package is provides helper function for testing with the standard library.
+//
+// Comment on the assertion line are used to add a description.
+//
+// The following failing test:
+//
+//		func TestFail(t *testing.T) {
+// 			// always start tests with this
+//			is := is.New(t)
+// 			a, b := 1, 2
+// 			is.Equal(a, b) // expect to be the same
+//		}
+//
+// Will output:
+//
+// 		1 != 2 // expect to be the same
 package is
 
 import (
@@ -24,6 +40,16 @@ func New(t T) *Is {
 }
 
 // Equal asserts that a and b are equal.
+//
+// 		func TestEqual(t *testing.T) {
+// 			is := is.New(t)
+// 			got := hello("world").
+// 			is.Equal(got, "hello world") // greeting the world
+// 		}
+//
+// Will output:
+//
+// 		wassup world := hello world // greeting the world
 func (is *Is) Equal(a, b interface{}) {
 	is.t.Helper()
 	if reflect.DeepEqual(a, b) {
@@ -86,6 +112,16 @@ func (is *Is) loadComment() string {
 }
 
 // NoErr assert that err is nil.
+//
+// 		func TestNoErr(t *testing.T) {
+//			is := is.New(t)
+// 			_, err := findGirlfriend("Anyone?")
+// 			is.NoErr(err) // poor you
+// 		}
+//
+// Will output:
+//
+// 		err: girlfriend not found // poor you
 func (is *Is) NoErr(err error) {
 	is.t.Helper()
 	if err != nil {
@@ -95,6 +131,16 @@ func (is *Is) NoErr(err error) {
 
 // True asserts that expression is true.
 // The expression code itself will be reported if the assertion fails.
+//
+// 		func TestTrue(t *testing.T) {
+// 			is := is.New(t)
+// 			val := wallet()
+// 			is.True(wallet != 0) // wallet should not be 0
+// 		}
+//
+// Will output:
+//
+// 		wallet != 0 // wallet should not be 0
 func (is *Is) True(expression bool) {
 	is.t.Helper()
 	if expression {
